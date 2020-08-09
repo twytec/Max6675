488918e2bb87de789e07882ed40565f8b516d69e
Thomas Wycichowski <wycichowski@live.de>
2020-08-09 21:22:07 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using Max6675;
+using System;
+using System.Device.Spi;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Max6675!");
+
+
+            GetTemp();
+            Console.ReadLine();
+        }
+
+        static async void GetTemp()
+        {
+            var connectionSettings = new SpiConnectionSettings(0, 0)
+            {
+                ClockFrequency = Max6675.Max6675.SpiClockFrequency,
+                Mode = SpiMode.Mode0
+            };
+
+            var spi = SpiDevice.Create(connectionSettings);
+            using var max = new Max6675.Max6675(spi);
+
+            while (true)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine($"Temperature in celsius is: {max.Celsius()}");
+            }
+        }
+    }
+}
